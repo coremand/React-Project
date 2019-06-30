a674abf7b498e787b97c8ae567155845bee5783e
val del <valinevite@gmail.com>
2019-06-29 18:38:06 -0600

    initial commit

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends Component {
+    render(){
+        return (
+        <div className="BusinessList">
+            {this.props.businesses.map(business => {
+                return <Business business= {business}/>;
+            })}
+        </div>
+        )
+    }
+}
+
+export default BusinessList;
